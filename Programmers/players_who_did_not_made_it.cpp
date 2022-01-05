f8e499dd2ee324e4355c677fc0f6c709a9cdcb04
teethemoji <idoseekim@knou.ac.kr>
2022-01-05 11:03:41 +0900

    Create players_who_did_not_made_it.cpp

---
+++ Programmers/players_who_did_not_made_it.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+
+    // 문제를 풉시다! 
+
+    string answer = "";
+    return answer;
+}
