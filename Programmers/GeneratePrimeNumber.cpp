b1e279b8b958ede38a40eaaf44e66cfceca0e49f
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-01-13 09:44:47 +0900

    Create GeneratePrimeNumber.cpp

---
+++ Programmers/GeneratePrimeNumber.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> nums) {
+    int answer = -1;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
